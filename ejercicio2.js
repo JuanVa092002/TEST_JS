eebda810bc420f669c1a0168709abe9b243ed4d6
JuanVa092002 <juan.pastasvalencia@hotmail.com>
2026-07-01 10:10:58 -0500

    Repo init

---
+++ ejercicio2.js
@@ -0,0 +1,10 @@
+// for (let index = 1; index <= 10; index++) {
+//     const numero = 7*index;
+//     console.log(`7 x ${index} = ${numero}`);
+// }
+
+for (let paso = 1; paso <= 10; paso++) {
+  const resultado = paso * 7
+    // console.log(`7 x ${paso} = ${resultado}`)
+    console.log('7 x ' + paso + ' = ' + resultado);
+}
