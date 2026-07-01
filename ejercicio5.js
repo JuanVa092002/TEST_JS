d475d173f87dea249f3f43abdd266da29dbe2e91
JuanVa092002 <juan.pastasvalencia@hotmail.com>
2026-07-01 10:31:22 -0500

    exercise5

---
+++ ejercicio5.js
@@ -0,0 +1,5 @@
+let dias = 5  
+let ventas = [120000,95000,150000,80000,200000]
+// Cual fue la venta mas alta
+let ventaAlta = Math.max(...ventas)
+console.log(`La venta mas alta fue: ${ventaAlta}`)
