63d16d739963799de1154da46fe93054c39fe271
JuanVa092002 <juan.pastasvalencia@hotmail.com>
2026-07-01 10:26:28 -0500

    4

---
+++ ejercicio4.js
@@ -0,0 +1,7 @@
+let dias = 5  
+let ventas = [120000,95000,150000,80000,200000]
+let sumaVentas = (120000+95000+150000+80000+200000) / 5
+    console.log(`El promedio de ventas es: ${sumaVentas}`)
+
+// let promedioVentas = ventas.reduce((a,b) => a + b, 0) / dias
+// console.log(`El promedio de ventas es: ${promedioVentas}`) 
